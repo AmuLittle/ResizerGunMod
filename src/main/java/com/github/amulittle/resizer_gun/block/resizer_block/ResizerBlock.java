215bc71b6876e39e5de1b60822026c8081d0b566
Amu <110311878+AmuLittle@users.noreply.github.com>
2024-07-11 21:08:50 -0400

    Start work on ResizerBlock

---
+++ src/main/java/com/github/amulittle/resizer_gun/block/resizer_block/ResizerBlock.java
@@ -0,0 +1,11 @@
+package com.github.amulittle.resizer_gun.block.resizer_block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+
+public class ResizerBlock extends Block {
+    
+    public ResizerBlock(AbstractBlock.Settings settings) {
+        super(settings);
+    }
+}
